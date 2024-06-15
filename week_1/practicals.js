ca6b805d04f0810a442fed7cc368746207dba045
Nimna Pathum <113501913+Nimnapathum@users.noreply.github.com>
2024-06-15 23:03:37 +0530

    Add some conversions on js practicals

---
+++ week_1/practicals.js
@@ -0,0 +1,56 @@
+//Strings to Numbers
+let str1 = "42";
+console.log(Number(str1));
+
+let str2 = "3.1415";
+console.log(Number(str2));
+
+let str3 = "0";
+console.log(Number(str3));
+
+let str4 = "    56    ";
+console.log(Number(str4));
+
+let str5 = "123abc";
+console.log(Number(str5));
+
+let str6 = "    ";
+console.log(Number(str6));
+
+//Bool to Numbers
+let bool1 = true;
+console.log(Number(bool1));
+
+let bool2 = false;
+console.log(Number(bool2));
+
+//Dates to Numbers
+let date1 = new Date("2023-12-25");
+console.log(Number(date1));
+
+let date2 = new Date("1970-01-01");
+console.log(Number(date2));
+
+//Numbers to Strings
+let num1 = 123;
+console.log(String(num1));
+
+let num2 = 4.567;
+console.log(String(num2));
+
+let num3 = 0;
+console.log(String(num3));
+
+let num4 = -999;
+console.log(String(num4));
+
+//let num5 = le10;
+//console.log(String(num5));
+
+//Boolean to Strings
+console.log(String(bool1));
+console.log(String(bool2));
+
+//Dates to Strings
+console.log(String(date1));
+console.log(String(date2));
